ae3707fa072b9d8bc11e902138a773283d3e820a
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-04-03 13:53:54 +0700

    feat: setup note layout

---
+++ pages/note/index.vue
@@ -0,0 +1,103 @@
+<!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<template>
+  <!-- When the mobile menu is open, add `overflow-hidden` to the `body` element to prevent double scrollbars -->
+  <Popover as="template" v-slot="{ open }">
+    <header
+      :class="[
+        open ? 'fixed inset-0 z-40 overflow-y-auto' : '',
+        'bg-white shadow-sm lg:static lg:overflow-y-visible',
+      ]"
+    >
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div
+          class="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8"
+        >
+          <div
+            class="flex md:absolute md:left-0 md:inset-y-0 lg:static xl:col-span-2"
+          >
+            <div class="flex-shrink-0 flex items-center">
+              <a href="#">
+                <div class="text-xl">My Notes</div>
+              </a>
+            </div>
+          </div>
+          <div class="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6">
+            <div
+              class="flex items-center px-6 py-4 md:max-w-3xl md:mx-auto lg:max-w-none lg:mx-0 xl:px-0"
+            >
+              <div class="w-full">
+                <label for="search" class="sr-only">Search</label>
+                <div class="relative">
+                  <div
+                    class="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center"
+                  >
+                    <Icon class="h-5 w-5 text-gray-400" name="uil:search" />
+                  </div>
+                  <input
+                    id="search"
+                    name="search"
+                    class="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    placeholder="Search"
+                    type="search"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="flex items-center justify-end col-span-4"
+          >
+            <a
+              href="#"
+              class="ml-6 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              <Icon name="uil:plus" class="w-5 h-5 bg-indigo-600"/>
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  </Popover>
+</template>
+
+<script>
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+} from "@headlessui/vue";
+
+export default {
+  components: {
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    Popover,
+    PopoverButton,
+    PopoverPanel,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
